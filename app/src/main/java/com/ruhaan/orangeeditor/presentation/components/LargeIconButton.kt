11b0829971ff96371a4d59a4f2b00e2912c12320
Manish Jajoriya <manishjajoriya2005@gmail.com>
2025-12-31 14:45:45 +0530

    Move LargeIconButton.kt from editor/components -> presentation/components

--- app/src/main/java/com/ruhaan/orangeeditor/presentation/components/LargeIconButton.kt
+++ app/src/main/java/com/ruhaan/orangeeditor/presentation/components/LargeIconButton.kt
@@ -1,4 +1,4 @@
-package com.ruhaan.orangeeditor.presentation.editor.components
+package com.ruhaan.orangeeditor.presentation.components
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
