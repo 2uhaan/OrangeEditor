b3cdca086f67fad7e9a6a471da53571bced06800
Manish Jajoriya <manishjajoriya2005@gmail.com>
2026-01-01 17:05:16 +0530

    add java doc for text layer

--- app/src/main/java/com/ruhaan/orangeeditor/domain/model/layer/TextLayer.kt
+++ app/src/main/java/com/ruhaan/orangeeditor/domain/model/layer/TextLayer.kt
@@ -1,14 +1,18 @@
 package com.ruhaan.orangeeditor.domain.model.layer
 
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 
 data class TextLayer(
     override val id: String,
     val text: String,
     val color: Color,
-    val fontSizeInPx: Float,
-    val fontFamily: FontFamily,
+    val fontSizeInPx: Int,
+    /** This only support only Normal and Bold. */
+    val fontWeight: FontWeight,
+    /** This only support only Normal and Italic. */
+    val fontStyle: FontStyle,
     override val transform: Transform,
     override val zIndex: Int,
     override val visible: Boolean = true,
