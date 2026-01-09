15c4c8bb683e0b7cb1f7572a76c4baef17e62373
Manish Jajoriya <manishjajoriya2005@gmail.com>
2026-01-09 21:54:31 +0530

    added repository perform CRUD operation

---
+++ app/src/main/java/com/ruhaan/orangeeditor/data/repository/OrangeRepositoryImpl.kt
@@ -0,0 +1,81 @@
+package com.ruhaan.orangeeditor.data.repository
+
+import com.ruhaan.orangeeditor.data.dao.EditorStateDao
+import com.ruhaan.orangeeditor.data.dao.ImageLayerDao
+import com.ruhaan.orangeeditor.data.dao.TextLayerDao
+import com.ruhaan.orangeeditor.data.entity.EditorStateEntity
+import com.ruhaan.orangeeditor.data.mapper.toDomain
+import com.ruhaan.orangeeditor.data.mapper.toEntity
+import com.ruhaan.orangeeditor.domain.model.layer.EditorState
+import com.ruhaan.orangeeditor.domain.model.layer.ImageLayer
+import com.ruhaan.orangeeditor.domain.model.layer.TextLayer
+import com.ruhaan.orangeeditor.domain.repository.OrangeRepository
+import com.ruhaan.orangeeditor.util.AppStorage
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.withContext
+
+class OrangeRepositoryImpl(
+    private val editorStateDao: EditorStateDao,
+    private val imageLayerDao: ImageLayerDao,
+    private val textLayerDao: TextLayerDao,
+    private val appStorage: AppStorage,
+) : OrangeRepository {
+
+  override fun getAllEditorState(): Flow<List<EditorStateEntity>> =
+      editorStateDao.getAllEditorStateEntity()
+
+  override suspend fun getEditorStatById(editorId: String): EditorState =
+      withContext(Dispatchers.IO) {
+        val editorStateEntity = editorStateDao.getEditorStateEntityById(editorId = editorId)
+        val allImageLayerEntity = imageLayerDao.getImagerLayerByEditorStateId(editorId = editorId)
+        val allTextLayerEntity = textLayerDao.geTextLayerByEditorStateId(editorId = editorId)
+
+        val allImageLayer = coroutineScope {
+          allImageLayerEntity
+              .map { entity ->
+                async { entity.toDomain(appStorage.loadBitmapFromPath(entity.bitmapStoredPath)) }
+              }
+              .awaitAll()
+        }
+
+        val allTextLayer = allTextLayerEntity.map { textLayerEntity -> textLayerEntity.toDomain() }
+        val combineList = (allImageLayer + allTextLayer).sortedBy { it.zIndex }
+        val editorState = editorStateEntity.toDomain(layers = combineList)
+        return@withContext editorState
+      }
+
+  override suspend fun saveEditorState(editorState: EditorState) =
+      withContext(Dispatchers.IO) {
+        val editorStateEntity = editorState.toEntity()
+        editorStateDao.saveEditorStateEntity(editorStateEntity = editorStateEntity)
+        editorState.layers.forEach { layer ->
+          when (layer) {
+            is ImageLayer -> {
+              layer.bitmap?.let { nonNumBitmap ->
+                val bitmapPath = appStorage.saveBitmapToAppStorage(bitmap = nonNumBitmap)
+                bitmapPath?.let {
+                  val imageLayerEntity =
+                      layer.toEntity(editorState.editorId, bitmapPath = bitmapPath)
+                  imageLayerDao.saveImageLayerEntity(imageLayerEntity = imageLayerEntity)
+                }
+              }
+            }
+            is TextLayer -> {
+              val textLayerEntity = layer.toEntity(editorState.editorId)
+              textLayerDao.saveTextLayerEntity(textLayerEntity = textLayerEntity)
+            }
+          }
+        }
+      }
+
+  override suspend fun deleteEditorStateById(editorId: String) =
+      withContext(Dispatchers.IO) {
+        val allImageLayerEntity = imageLayerDao.getImagerLayerByEditorStateId(editorId)
+        allImageLayerEntity.forEach { appStorage.deleteBitmapFromPath(it.bitmapStoredPath) }
+        editorStateDao.deleteEditorStateEntity(editorId)
+      }
+}
