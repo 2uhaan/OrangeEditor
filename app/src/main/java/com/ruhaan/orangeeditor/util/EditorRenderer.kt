d2244e4d013971dffd6dff5222c1762304f98d71
Manish Jajoriya <manishjajoriya2005@gmail.com>
2026-01-16 15:07:54 +0530

    added spacing between selected image and border and fix delay in drawing rectangle when text scale

--- app/src/main/java/com/ruhaan/orangeeditor/util/EditorRenderer.kt
+++ app/src/main/java/com/ruhaan/orangeeditor/util/EditorRenderer.kt
@@ -31,7 +31,7 @@ class EditorRenderer {
         .filter { it.visible }
         .forEach { layer ->
           when (layer) {
-            is TextLayer -> drawText(canvas, layer, scaleX, scaleY, selectedLayerId,onTextMeasured)
+            is TextLayer -> drawText(canvas, layer, scaleX, scaleY, selectedLayerId, onTextMeasured)
             is ImageLayer -> drawImage(canvas, layer, scaleX, scaleY, selectedLayerId)
           }
         }
@@ -77,7 +77,7 @@ class EditorRenderer {
           val borderPaint =
               Paint().apply {
                 style = Paint.Style.STROKE
-                strokeWidth = 4f
+                strokeWidth = 8f
                 color = CanvasOrange.toArgb()
                 isAntiAlias = true
               }
@@ -86,7 +86,7 @@ class EditorRenderer {
           val halfW = layer.bitmap.width / 2f
           val halfH = layer.bitmap.height / 2f
 
-          drawRect(-halfW, -halfH, halfW, halfH, borderPaint)
+          drawRect(-(halfW + 10), -(halfH + 10), (halfW + 10), (halfH + 10), borderPaint)
         }
       }
     }
@@ -140,7 +140,7 @@ class EditorRenderer {
               isAntiAlias = true
             }
 
-        drawRect(0f, 0f, layer.textWidthPx.toFloat(), layer.textHeightPx.toFloat(), borderPaint)
+        drawRect(0f, 0f, bounds.width().toFloat(), bounds.height().toFloat(), borderPaint)
       }
     }
   }
